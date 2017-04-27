abfd791bca00e510fff7bd1013a225917ae3b0b3
Makoto Otsuka <mametra2580@gmail.com>
2017-04-27 19:09:31 +0900

    add tweets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/new'
+  post 'tweets' => "tweets#create"
+
   get 'users/index'
 
   get 'users/show/:username' => "users#show"
